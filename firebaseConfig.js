d67a4784983f677873872e0bf19e002f6cbee077
Anna <djepko3@gmail.com>
2025-05-31 00:20:49 +0200

    firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCMv_ii5NEoAOl6wc67ACXRdtj8g3iLNrA",
-  authDomain: "cleaningproject-18b2a.firebaseapp.com",
-  projectId: "cleaningproject-18b2a",
-  storageBucket: "cleaningproject-18b2a.appspot.com",
-  messagingSenderId: "234197222012",
-  appId: "1:234197222012:web:d5242d104652f64b5c7ff9",
-  measurementId: "G-DCD20GDC3G"
+  apiKey: "///",
+  authDomain: "///",
+  projectId: "///",
+  storageBucket: "///",
+  messagingSenderId: "///",
+  appId: "///",
+  measurementId: "///"
 };
 
 const app = initializeApp(firebaseConfig);
